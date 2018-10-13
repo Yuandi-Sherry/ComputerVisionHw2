f055d2d65f18d0a1b97db173607ab8d56ea3efc7
周远笛 <1094050886@qq.com>
2018-10-13 13:46:52 +0800

    完成

---
+++ main.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  Canny Edge Detector
+//
+//  Created by Hasan Akgün on 21/03/14.
+//  Copyright (c) 2014 Hasan Akgün. All rights reserved.
+//
+
+/*#include <iostream>
+#define _USE_MATH_DEFINES
+
+#include <cmath>
+#include <vector>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "canny.h"
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    cv::String filePath = "lena.jpg"; //Filepath of input image
+    canny cny(filePath);
+        
+    return 0;
+}
+*/
+#include "canny.h"
+int main()
+{
+	char const* filePath = "lena.bmp";
+	canny answer = canny(filePath);
+	system("pause");
+}
